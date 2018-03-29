192dcaa717b92bdd918a110a1d402a068a3e91d8
kianel <l.kianel@gmail.com>
2018-03-29 19:19:47 +0300

    timeouts

---
+++ src/test/java/lesson06/e_explicit/FirstTest.java
@@ -0,0 +1,49 @@
+package lesson06.e_explicit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElement;
+
+
+public class FirstTest {
+    static WebDriver driver;
+    @BeforeClass
+    public static void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+        driver.get("http://automationpractice.com/index.php");
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();}
+
+    @Test
+    public void firstResultShouldContainExpectedText() {
+        String query1 = "Dress";
+        driver.findElement(By.id("search_query_top")).click();
+        driver.findElement(By.id("search_query_top")).sendKeys(query1);
+        (new WebDriverWait(driver,10)).until(textToBePresentInElement(driver.findElement(By.xpath("//*[@id='index']/div[2]/ul/li[1]")),query1));
+        driver.findElement(By.id("search_query_top")).clear();
+
+
+        String query2 = "T-shirt";
+        driver.findElement(By.id("search_query_top")).click();
+        driver.findElement(By.id("search_query_top")).sendKeys(query2);
+        (new WebDriverWait(driver,10)).until(textToBePresentInElement(driver.findElement(By.xpath("//*[@id='index']/div[2]/ul/li[1]")),query2));
+    }
+}
+
+
